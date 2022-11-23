24d03b12a6046f56036ed3d00db4a2b0bd9f71be
maimas <maimas.andrei@gmail.com>
2022-11-22 20:02:21 -0800

    chore: added limits to V5.1

--- src/test/java/com/mg/trading/boot/tbd/charts/DEMAV5CandlestickChart.java
+++ src/test/java/com/mg/trading/boot/tbd/charts/DEMAV5CandlestickChart.java
@@ -1,11 +1,10 @@
 package com.mg.trading.boot.tbd.charts;
 
 import com.mg.trading.boot.domain.indicators.supertrentv2.SuperTrend;
-import com.mg.trading.boot.domain.strategy.dema5.DEMAStrategyDefinitionV5;
+import com.mg.trading.boot.domain.strategy.dema5.DEMAStrategyDefinitionV5_1;
 import com.mg.trading.boot.tbd.TestDataProvider;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.ta4j.core.BarSeries;
-import org.ta4j.core.indicators.ChandelierExitLongIndicator;
 import org.ta4j.core.indicators.DoubleEMAIndicator;
 import org.ta4j.core.indicators.bollinger.BollingerBandFacade;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
@@ -29,19 +28,17 @@ public class DEMAV5CandlestickChart {
         DoubleEMAIndicator shortDEMA = new DoubleEMAIndicator(closePrice, 10);
         DoubleEMAIndicator longDEMA = new DoubleEMAIndicator(closePrice, 60);
         BollingerBandFacade bollinger = new BollingerBandFacade(series, 60, 3.5);
-        ChandelierExitLongIndicator chandLong = new ChandelierExitLongIndicator(series, 5, 3);
         SuperTrend superTrend = new SuperTrend(series, 10, 3D);
 
         dataset.addSeries(buildChartBarSeries(series, shortDEMA, "S_DEMA"));
         dataset.addSeries(buildChartBarSeries(series, longDEMA, "L_DEMA"));
         dataset.addSeries(buildChartBarSeries(series, bollinger.upper(), "B_H"));
         dataset.addSeries(buildChartBarSeries(series, bollinger.lower(), "B_L"));
-        dataset.addSeries(buildChartBarSeries(series, chandLong, "Chand"));
         dataset.addSeries(buildChartBarSeries(series, superTrend, "ST"));
 
 
         CandlestickChart chart = new CandlestickChart();
-        chart.display(series, dataset, new DEMAStrategyDefinitionV5(symbol));
+        chart.display(series, dataset, new DEMAStrategyDefinitionV5_1(symbol));
     }
 
 }
