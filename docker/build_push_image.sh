8a030bc134df6abc3f7ad94458348014aef19ab5
Vlad Munteanu <munteanu.vladislav89@gmail.com>
2022-11-27 22:57:28 -0800

    updated docker file and docker build scripts to share the logs by mountig a system folder on the container

--- docker/build_push_image.sh
+++ docker/build_push_image.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 image_name="tb-app"
 image_tag_name="latest"
@@ -6,45 +7,44 @@ exit_status=0
 
 if [ "$1" ]; then
   image_tag_name=$1
-else
-  image_tag_name="latest"
 fi
 
 #step 1
-echo "[INFO] build new package"
+echo "[INFO] Build new package"
 mvn clean package -D skipTests
 
 #step 2
 exit_status=$?
-if [ $exit_status -eq 0 ];
-then
-  echo "[INFO] build docker image: $image_name"
-  docker build . -t $image_name:$image_tag_name -f docker/Dockerfile
+if [ $exit_status -eq 0 ]; then
+  echo "[INFO] Build docker image: $image_name"
+  if [ "$2" ]; then
+    docker buildx build . --platform "$2" -t $image_name:"$image_tag_name" -f docker/Dockerfile
+  else
+    docker build . -t $image_name:"$image_tag_name" -f docker/Dockerfile
+  fi
   exit_status=$?
 else
-  echo "[ERROR] failure at step 1"
+  echo "[ERROR] Failure at step 1"
   exit 1
 fi
 
 #step 3
-if [ $exit_status -eq 0 ];
-then
-  echo "[INFO] login into GitHub Container Repository"
-  echo $CR_PAT | docker login ghcr.io -u $GH_USER --password-stdin
+if [ $exit_status -eq 0 ]; then
+  echo "[INFO] Login into GitHub Container Repository"
+  echo "$CR_PAT" | docker login ghcr.io -u "$GH_USER" --password-stdin
   exit_status=$?
 else
-  echo "[ERROR] failure at step 2"
+  echo "[ERROR] Failure at step 2"
   exit 1
 fi
 
 #step 4
-if [ $exit_status -eq 0 ];
-then
-  echo "[INFO] push docker image to $repo_name/$image_name:$image_tag_name"
-  docker tag $image_name:$image_tag_name ghcr.io/$repo_name/$image_name:$image_tag_name
-  docker push ghcr.io/$repo_name/$image_name:$image_tag_name
+if [ $exit_status -eq 0 ]; then
+  echo "[INFO] Push docker image to $repo_name/$image_name:$image_tag_name"
+  docker tag $image_name:"$image_tag_name" ghcr.io/$repo_name/$image_name:"$image_tag_name"
+  docker push ghcr.io/$repo_name/$image_name:"$image_tag_name"
   exit_status=$?
 else
-  echo "[ERROR] failure at step 3"
+  echo "[ERROR] Failure at step 3"
   exit 1
 fi
